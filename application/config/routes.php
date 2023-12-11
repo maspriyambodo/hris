1fc98959ff369a391df70cc0ad61f4171975d143
masbodo <maspriyambodo@gmail.com>
2023-12-11 10:17:39 +0700

    remove unused routes

--- application/config/routes.php
+++ application/config/routes.php
@@ -3,23 +3,8 @@
 defined('BASEPATH') or exit('No direct script access allowed');
 $route['default_controller'] = 'Auth';
 $route['Change%(:any)'] = 'Systems/Change/';
-$route['keluaran_detail'] = 'Applications/Toto/Keluaran/Detail/';
 $route['Setting%20Profile'] = 'Systems/Profile/';
 $route['Dashboard'] = 'Applications/Dashboard/index/';
 $route['Signin'] = 'Auth/index/';
 $route['404_override'] = 'Error_404';
-//=====================================finance========================================
-$route['finance'] = 'Applications/Finance/Dashboard/index/';
-$route['finance-update'] = 'Applications/Finance/Dashboard/Update/';
-$route['finance-delete'] = 'Applications/Finance/Dashboard/Delete/';
-//=====================================finance========================================
-//=====================================idx========================================
-$route['company-profiles'] = 'Idx/Compro/index/';
-$route['trading-info-daily'] = 'Applications/Finance/Dashboard/index/';
-//=====================================idx========================================
 $route['translate_uri_dashes'] = false;
-$route['bkm-upload'] = 'Bkm/Upload';
-$route['bkm-save'] = 'Bkm/Save';
-$route['bkm-getkab'] = 'Bkm/Getkab';
-$route['bkm-getkec'] = 'Bkm/Getkec';
-$route['bkm-getkel'] = 'Bkm/Getkel';
